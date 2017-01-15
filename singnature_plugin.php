ece2ce0d7a210074be24f67c5970bd3b6e891737
Ben Rush <orionrush@gmail.com>
2017-01-15 19:17:44 +0000

    Updated the constant names to remove the TR reference

--- singnature_plugin.php
+++ singnature_plugin.php
@@ -16,30 +16,30 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 /***********************************************************************
  * Definitions
  * /*********************************************************************/
-define( 'TRSIG_PATH', plugin_dir_path( __FILE__ ) );
-define( 'TRSIG_URL', plugin_dir_url( __FILE__ ) );
+define( 'SIGNATURE_PATH', plugin_dir_path( __FILE__ ) );
+define( 'SIGNATURE_URL', plugin_dir_url( __FILE__ ) );
 
 /***********************************************************************
  * Includes
  * /*********************************************************************/
 //TinyMCE button
-require_once( TRSIG_PATH . 'lib/tinyMCE.php' );
+require_once( SIGNATURE_PATH . 'lib/tinyMCE.php' );
 
 //Admin Page - We've not finished it, and so far (oddly) it's interfering with the Featured Image functionality on posts and pages.
-//require_once ( TRSIG_PATH . 'lib/admin.php');
+//require_once ( SIGNATURE_PATH . 'lib/admin.php');
 
 
 /*
  * Generate the shortcode
  *
  * @uses:   shortcode_atts()
- * @uses:   const TRSIG_URL
+ * @uses:   const SIGNATURE_URL
  * @param:  string $content // the text between the opening and closing shortcode elements
  * @param:  array $atts // author, image, height, width
  * @return  markup as a string
  */
 function trsig_shortcode( $atts, $content = 'All the best,' ) {
-	$path = TRSIG_URL . 'img/signature.png';
+	$path = SIGNATURE_URL . 'img/signature.png';
 	$a    = shortcode_atts( array(
 		'author' => 'Tom',
 		'image'  => $path,
